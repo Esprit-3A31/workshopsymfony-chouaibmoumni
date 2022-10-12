3fa8f29fec7977b079f5bfc37121ac1d13b03774
chouaibmoumni <77495195+chouaibmoumni@users.noreply.github.com>
2022-10-12 19:13:19 +0100

    Add files via upload

---
+++ src/Entity/Student.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $nce = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNce(): ?string
+    {
+        return $this->nce;
+    }
+
+    public function setNce(string $nce): self
+    {
+        $this->nce = $nce;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+}
